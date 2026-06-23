cdfb71a99e982206683e32e82c50043f9ac7f5a5
Kaf <coffeechopdev@gmail.com>
2026-06-23 14:00:40 -0300

    Adopt multiloader build tools 2.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.kikugie.stonecutter") version "0.9.2"
     id("com.iamkaf.multiloader.settings") version providers.gradleProperty("project.plugins").get()
 }
