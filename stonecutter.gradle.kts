96fa3362f0a938b3f35bafb4d9b322f42ea03aa4
Kaf <coffeechopdev@gmail.com>
2026-08-13 10:33:25 -0300

    Update TeaKit to 0.14.0

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     id("fabric-loom") apply false
     id("net.fabricmc.fabric-loom") apply false
     id("com.iamkaf.multiloader.root")
-    id("com.iamkaf.teakit") version "0.13.2"
+    id("com.iamkaf.teakit") version "0.14.0"
 }
 
 teakit {
-    runnerVersion.set("0.13.2")
+    runnerVersion.set("0.14.0")
     testDirectories.add("test/teakit")
     timeoutSeconds.set(720)
     failOnRuntimeIncomplete.set(true)
